a649a599f70af3ac87e501e30ca8d62da70fcb44
Abdelkrim Bournane <ea_bournane@esi.dz>
2022-01-08 13:34:03 +0100

    Updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1429,9 +1429,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "96824553ed31a24778eba518c8cff4cc",
-"/": "96824553ed31a24778eba518c8cff4cc",
-"main.dart.js": "0573bee12c17edf1d470294f58e7e7dc",
+"index.html": "31ee11b41f8bb693140ab01e96d33976",
+"/": "31ee11b41f8bb693140ab01e96d33976",
+"main.dart.js": "d226dfddad88758e0531d7016389960b",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
